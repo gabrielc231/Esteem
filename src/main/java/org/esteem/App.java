f5aee1d8c6383d7f074cbe53600bb7be163eec5f
gabrielc231 <142500547+gabrielc231@users.noreply.github.com>
2024-05-25 21:56:42 -0300

    Erro "package org.esteem.view does not exist" corrigido

--- src/main/java/org/esteem/App.java
+++ src/main/java/org/esteem/App.java
@@ -6,14 +6,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner sc1 = new Scanner(System.in);
-        LojaView v = new LojaView();
         System.out.println( "Hello World!" );
-        v.showLojaOptions();
-        if (sc1.nextLine().equals("clear")) {
-            v.clearConsole();
-        }
-        System.out.println(":)");
-        sc1.close();
     }
 }
